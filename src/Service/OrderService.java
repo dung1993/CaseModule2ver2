2328be3114cad9405b9d1b1f43d0be4e2107bf2d
dung1993 <crorstar1704@gmail.com>
2022-10-28 10:54:29 +0700

    thuchanh

--- src/Service/OrderService.java
+++ src/Service/OrderService.java
@@ -1,5 +1,6 @@
 package src.Service;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
 import src.Raw.Order;
 import src.Utils.CSVUtils;
 
@@ -7,22 +8,23 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderService implements IOrderService{
+public class OrderService implements IOrderService {
 
     public static String Path = "D:\\CaseModule2ver2\\data\\Orders";
     private static OrderService instanceOrder;
 
-    public static OrderService getInstanceOrder(){
-        if (instanceOrder == null){
+    public static OrderService getInstanceOrder() {
+        if (instanceOrder == null) {
             instanceOrder = new OrderService();
         }
         return instanceOrder;
     }
+
     @Override
     public List<Order> findALlOrders() {
         List<Order> orders = new ArrayList<>();
         List<String> records = CSVUtils.read(Path);
-        for (String record : records){
+        for (String record : records) {
             orders.add(Order.parseOrder(record));
         }
         return orders;
@@ -33,23 +35,23 @@ public class OrderService implements IOrderService{
         List<Order> orders = findALlOrders();
         newOrder.setOrderTime(Instant.now());
         orders.add(newOrder);
-        CSVUtils.write(Path,orders);
+        CSVUtils.write(Path, orders);
     }
 
     @Override
     public void edit(Order newOrder) {
         List<Order> orders = findALlOrders();
-        for (Order oldOrder: orders) {
-            if (oldOrder.getIdOrder().equals(newOrder.getIdOrder())){
+        for (Order oldOrder : orders) {
+            if (oldOrder.getIdOrder().equals(newOrder.getIdOrder())) {
                 String name = newOrder.getFullName();
-                if (name != null && !name.isEmpty()){
+                if (name != null && !name.isEmpty()) {
                     oldOrder.setFullName(newOrder.getFullName());
                 }
                 String phoneNumber = newOrder.getPhoneNumber();
-                if (phoneNumber != null && !phoneNumber.isEmpty()){
+                if (phoneNumber != null && !phoneNumber.isEmpty()) {
                     oldOrder.setPhoneNumber(newOrder.getPhoneNumber());
                 }
-                CSVUtils.write(Path,orders);
+                CSVUtils.write(Path, orders);
                 break;
             }
         }
@@ -59,18 +61,18 @@ public class OrderService implements IOrderService{
     public void remove(Long idOrder) {
         List<Order> orders = findALlOrders();
         orders.removeIf(id -> id.getIdOrder().equals(idOrder));
-        CSVUtils.write(Path,orders);
+        CSVUtils.write(Path, orders);
     }
 
     @Override
     public Order findOrderById(Long idOrder) {
         List<Order> orders = findALlOrders();
         List<String> listFind = new ArrayList<>();
-        if (idOrder > 0){
-            for (Order findOrder: orders) {
-              if (findOrder.getIdOrder().equals(idOrder)){
-                  listFind.add(findOrder.toString());
-              }
+        if (idOrder > 0) {
+            for (Order findOrder : orders) {
+                if (findOrder.getIdOrder().equals(idOrder)) {
+                    listFind.add(findOrder.toString());
+                }
             }
         }
         return null;
@@ -83,6 +85,13 @@ public class OrderService implements IOrderService{
 
     @Override
     public List<Order> findOrderByIdUser(Long idOrder) {
+        List<Order> order = findALlOrders();
+        for (Order orders: findALlOrders()) {
+            if (orders.getIdOrder().equals(idOrder)){
+                order.add(orders);
+            }
+            return order;
+        }
         return null;
     }
 }
